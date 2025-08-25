6493706ee91c753501810730f310a6bf065c83b6
lex <--global>
2025-08-25 17:56:38 +0800

    25-08-25

---
+++ src/455.分发饼干.ts
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=455 lang=typescript
+ *
+ * [455] 分发饼干
+ */
+
+// @lc code=start
+function findContentChildren(g: number[], s: number[]): number {
+  g.sort((a, b) => a - b)
+  s.sort((a, b) => a - b)
+  const childLength: number = g.length
+  const cookieLength: number = s.length
+  let curChild: number = childLength - 1
+  let curCookie: number = cookieLength - 1
+  let resCount: number = 0
+  while (curChild >= 0 && curCookie >= 0) {
+    if (g[curChild] <= s[curCookie]) {
+      curCookie--
+      resCount++
+    }
+    curChild--
+  }
+  return resCount
+};
+// @lc code=end
