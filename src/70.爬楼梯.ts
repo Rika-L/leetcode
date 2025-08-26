0901eda1b8363d89f109b7b6390720e71066d714
lex <rika00126@.icloud.com>
2025-08-26 20:44:42 +0800

    25-08-26

---
+++ src/70.爬楼梯.ts
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=70 lang=typescript
+ *
+ * [70] 爬楼梯
+ */
+
+// @lc code=start
+function climbStairs(n: number): number {
+  const dp = [1, 2]
+  for (let i = 2; i < n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2]
+  }
+  return dp[n - 1]
+};
+// @lc code=end
